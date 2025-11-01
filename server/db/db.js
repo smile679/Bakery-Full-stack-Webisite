49aadf12a4cf9a6f71209341c8b2ffa9b7e0ad47
samuel88 <samuelgide88@gmail.com>
2025-11-02 00:23:38 +0300

    db inistalation

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
-const connectToDb = mongoose.connect()
+const connectToDb =async()=> {
+  try{
+   await mongoose.connect(
+      "mongodb+srv://samuelgide88_backery_user:samuelgide88_backery_user@cluster0.y9l69jp.mongodb.net/"
+    )
+    
+    console.log("mongoose successfully connected")
+  } catch(e){
+    console.log(e);
+    process.getMaxListeners(1);
+  }
+};
+
+
+module.exports = connectToDb;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const connectToDb = require('./db/db');
 const app = express()
 const PORT = 5000
 
-
 app.use(express.json());
-
+connectToDb()
 
 app.listen(PORT , ()=>{
-  console.log("Server successfully started");
+  console.log("Server connected successfully");
 })
