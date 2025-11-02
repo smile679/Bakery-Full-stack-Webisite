abb40d4aedaa9964591ed5d0e41262deede20201
samuel88 <samuelgide88@gmail.com>
2025-11-02 22:36:54 +0300

    sdffjk

--- server/db/db.js
+++ server/db/db.js
@@ -2,16 +2,12 @@ const mongoose = require('mongoose');
 
 const connectToDb =async()=> {
   try{
-   await mongoose.connect(
-      "mongodb+srv://samuelgide88_backery_user:samuelgide88_backery_user@cluster0.y9l69jp.mongodb.net/"
-    )
-    
-    console.log("mongoose successfully connected")
+   await mongoose.connect(process.env.MONGOOSE_URL)
+   console.log("mongoose successfully connected")
   } catch(e){
     console.log(e);
     process.getMaxListeners(1);
   }
 };
-
 
 module.exports = connectToDb;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config
 const express = require('express');
 const connectToDb = require('./db/db');
 const app = express()
