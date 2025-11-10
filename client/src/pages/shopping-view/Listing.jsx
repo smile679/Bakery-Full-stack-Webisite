4a3a0320f9e4c2df233962a185e62a7308b8041b
samuel88 <samuelgide88@gmail.com>
2025-11-10 22:35:16 +0300

    listing

--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -1,10 +1,34 @@
+import FoodCard from "@/components/common/FoodCard";
+import { useSelector } from "react-redux";
 
 
 function Listing() {
+  const { productsList } = useSelector((state) => state.products);
+  const handleAddToCart = (e) => {
+    // Add your add to cart logic here
+  }
   return ( 
-    <div>
-      Listing Page
-    </div>
+    <section>
+      <div>
+        <h1 className="text-2xl sm:text-3xl font-bold font-sansita-swashed text-center text-[#111111] mt-10">
+          Product Listing
+        </h1>
+      </div>
+      <div className="max-w-7xl mx-auto grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-5 px-2 py-10 sm:mb-10">
+        {
+          productsList.slice(0, 9).map((bread, index) =>(
+            <FoodCard
+              key={index}
+              image={bread?.image}
+              title={bread?.title}
+              price={bread?.price}
+              btnText="Add to Cart"
+              handleClick={handleAddToCart}
+            />
+          ))
+        }
+      </div>
+    </section>
    );
 }
 
