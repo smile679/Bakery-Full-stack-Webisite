8a3c6c3f5a7331a78d045bb71e534e17472eb1a6
samuel88 <samuelgide88@gmail.com>
2025-11-17 12:18:05 +0300

    fkjll

--- client/src/pages/shopping-view/Cart.jsx
+++ client/src/pages/shopping-view/Cart.jsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
 import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
   Table,
   TableBody,
   TableCell,
@@ -13,7 +20,7 @@ import {
   fetchCartItems,
   updateCartItem,
 } from "@/store/cart-slice";
-import { Trash } from "lucide-react";
+import { MoveRight, Trash } from "lucide-react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
@@ -22,6 +29,10 @@ function Cart() {
   const { cartItems } = useSelector((state) => state.cart);
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+
+  const totalPrice = cartItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
 
   function handleDelete(productId) {
     dispatch(deleteCartItem({ userId: user?.id, productId })).then((data) => {
@@ -32,21 +43,24 @@ function Cart() {
     });
   }
 
-  function handleQuantity({ productId ,payload, quantity }) {
-    if(payload === "minus" && quantity <=1) return;
+  function handleQuantity({ productId, payload, quantity }) {
+    if (payload === "minus" && quantity <= 1) return;
     if (payload === "plus") {
-      dispatch(updateCartItem({ userId: user?.id, productId, quantity : quantity + 1 })).then(data=>{
-        if(data.payload?.success){
+      dispatch(
+        updateCartItem({ userId: user?.id, productId, quantity: quantity + 1 })
+      ).then((data) => {
+        if (data.payload?.success) {
           dispatch(fetchCartItems({ userId: user?.id }));
         }
-      }
-      )
+      });
     } else {
-      dispatch(updateCartItem({ userId: user?.id, productId, quantity : quantity - 1 })).then(data=>{
-        if(data.payload?.success){
+      dispatch(
+        updateCartItem({ userId: user?.id, productId, quantity: quantity - 1 })
+      ).then((data) => {
+        if (data.payload?.success) {
           dispatch(fetchCartItems({ userId: user?.id }));
         }
-      })
+      });
     }
   }
 
@@ -56,84 +70,124 @@ function Cart() {
 
   return (
     <section className="w-full min-h-screen py-15 px-2">
-      <h1 className="w-full text-3xl font-bold font-sansita-swashed mt-10 text-center">Your Cart</h1>
+      <h1 className="w-full text-3xl font-bold font-sansita-swashed mt-10 text-center">
+        Your Cart
+      </h1>
       <div className="max-w-6xl mx-auto flex max-md:flex-col py-10 gap-5">
         <div className="flex-2 rounded-md">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Product</TableHead>
-                <TableHead className="text-center">Price</TableHead>
-                <TableHead>Quantity</TableHead>
-                {/* <TableHead>Subtotal</TableHead> */}
-                <TableHead className="sr-only">Delete</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {cartItems &&
-                cartItems.map((product, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <div className="font-semibold flex max-md:flex-col items-center gap-2 w-10 ml-5">
-                        <img src={product.image} />
-                        <div>
-                          <h3>{product.title}</h3>
-                        </div>
-                      </div>
-                    </TableCell>
-                    <TableCell className="text-center">
-                      ${product.price}
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-1">
-                        <Button
-                          variant="outline"
-                          className="px-1 py-0 gap-0 h-5"
-                          onClick={() => handleQuantity({
-                              productId: product.productId,
-                              payload: "minus",
-                              quantity: product?.quantity,
-                            })
-                          }
-                        >
-                          -
-                        </Button>
-                        <h3>{product.quantity}</h3>
-                        <Button
-                          variant="outline"
-                          className="px-1 py-0 gap-0 h-5"
-                          onClick={() =>
-                            handleQuantity({
-                              productId: product.productId,
-                              payload: "plus",
-                              quantity: product?.quantity,
-                            })
-                          }
-                        >
-                          +
-                        </Button>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <Button
-                        variant="outline"
-                        onClick={() => handleDelete(product.productId)}
-                      >
-                        <Trash />
-                      </Button>
-                    </TableCell>
+          <Card>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Product</TableHead>
+                    <TableHead className="text-center">Price</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead className="sr-only">Delete</TableHead>
                   </TableRow>
-                ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={3}>Total</TableCell>
-                <TableCell className="text-right">$2000</TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
+                </TableHeader>
+                <TableBody className="">
+                  {cartItems &&
+                    cartItems.map((product, index) => (
+                      <TableRow key={index}>
+                        <TableCell>
+                          <div className="font-semibold flex max-md:flex-col items-center gap-2 w-10 ml-5">
+                            <img src={product.image} />
+                            <div>
+                              <h3>{product.title}</h3>
+                            </div>
+                          </div>
+                        </TableCell>
+                        <TableCell className="text-center">
+                          ${product.price}
+                        </TableCell>
+                        <TableCell>
+                          <div className="flex items-center gap-1">
+                            <Button
+                              variant="outline"
+                              className="px-1 py-0 gap-0 h-5"
+                              onClick={() =>
+                                handleQuantity({
+                                  productId: product.productId,
+                                  payload: "minus",
+                                  quantity: product?.quantity,
+                                })
+                              }
+                            >
+                              -
+                            </Button>
+                            <h3>{product.quantity}</h3>
+                            <Button
+                              variant="outline"
+                              className="px-1 py-0 gap-0 h-5"
+                              onClick={() =>
+                                handleQuantity({
+                                  productId: product.productId,
+                                  payload: "plus",
+                                  quantity: product?.quantity,
+                                })
+                              }
+                            >
+                              +
+                            </Button>
+                          </div>
+                        </TableCell>
+                        <TableCell>
+                          <Button
+                            variant="outline"
+                            onClick={() => handleDelete(product.productId)}
+                          >
+                            <Trash />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow className="bg-gray-950">
+                    <TableCell colSpan={3} className="text-white">
+                      Total
+                    </TableCell>
+                    <TableCell className="text-white">${totalPrice}</TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex-1 w-full max-w-3xl bg-amber-700">123</div>
+        <div className="flex-1 w-full max-w-3xl">
+          <Card>
+            <CardHeader>
+              <h2 className="flex felx-col font-semibold">Order Summary</h2>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="w-full flex flex-col gap-y-5">
+                <div className="w-full flex justify-between items-center">
+                  <h3 className="text-muted-foreground font-semibold">
+                    Subtotal
+                  </h3>
+                  <p className="text-muted-foreground">${totalPrice}</p>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <h3 className="text-muted-foreground font-semibold">
+                    Shopping
+                  </h3>
+                  <p className="text-muted-foreground">Free</p>
+                </div>
+                <div className="w-full flex items-center">
+                  <h3 className="text-muted-foreground font-semibold">
+                    Add Coupon Code
+                  </h3>
+                  <MoveRight />
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">CheckOut</Button>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </section>
   );
